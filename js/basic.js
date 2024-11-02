e16f3671ed475751af03128f415852d4b8b94bfa
Chang Hello14 <125748085+Modificationer-MDF@users.noreply.github.com>
2024-11-02 22:22:37 +0800

    Update basic.js

--- js/basic.js
+++ js/basic.js
@@ -9,9 +9,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const h2 = document.querySelector(".head2"); // h2 标签。
     const div = document.querySelector(".top"); // 包含 h1 和 h2 的 div 标签。
     const head = document.querySelector(".head"); // The Play Games 版本标签。
-    const btn1 = document.querySelector(".btn1"); // 第一个按钮。
-    const btn2 = document.querySelector(".btn2"); // 第二个按钮。
-    const btn3 = document.querySelector(".btn3"); // 第三个按钮。
 
     h1.addEventListener("animationend", (e) => {
         if (e.animationName === "-head1") {
@@ -31,11 +28,6 @@ document.addEventListener("DOMContentLoaded", () => {
         if (e.animationName === "-top") {
             console.warn("-top 动画结束。");
             console.warn("-head 动画结束。");
-            btn1.style.animation = "-btn1 0.7s forwards cubic-bezier(0.33, 1, 0.68, 1)";
-            btn2.style.animation = "-btn2 0.7s forwards cubic-bezier(0.33, 1, 0.68, 1)";
-            btn3.style.animation = "-btn3 0.7s forwards cubic-bezier(0.33, 1, 0.68, 1)";
-            btn2.style.opacity = 1;
-            btn3.style.opacity = 1;
         }
     });
 });
