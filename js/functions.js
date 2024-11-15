b856c7c21aab082dbee26ac6ddfcf8810d67d4af
Chang Hello14 <125748085+Modificationer-MDF@users.noreply.github.com>
2024-11-15 21:59:51 +0800

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,25 +1,71 @@
-var num = 0; // 窗口数量。
+var infoNum = 0;
+var warningNum = 0;
+var failNum = 0;
+var successNum = 0;
+var choiceNum = 0;
+var inputNum = 0;
+var transmitNum = 0;
+var linkNum = 0;
+var commandNum = 0;
+var nullCount = 0;
+var windows = []; // 窗口数组。
+let urlEndings = [
+    ".com", ".net", ".cn", ".org", ".edu", ".gov", ".mil",
+    ".biz", ".info", ".name", ".pro", ".aero", ".coop",
+    ".museum", ".jobs", ".travel", ".xxx", ".top", ".site",
+    ".wiki", ".cc", ".tv", ".mobi", ".me", ".bid", ".club",
+    ".online", ".store", ".work", ".tech", ".bid"
+];
 
 // 更新窗口位置的函数
-function updateSelectedWindowPos(wc) {
-    const windows = document.querySelectorAll(wc);
-    let total = 0;
-        windows.forEach((window, index) => {
-            const wh = window.offsetHeight; // 获取当前窗口的高度。
-            total += (index === 0 ? 0 : 7) + wh; // 计算此窗口的顶部位置。
-            // 设置位置过渡。
-            window.style.transition = "top 0.3s cubic-bezier(0.33, 1, 0.68, 1)";
-            if (index <= 2) {
-                window.style.top = `calc(${total}px + 8vh)`;
-            } else {
-                window.style.top = `calc(${total}px + 8vh)`; // 更新每个窗口的位置。
-            }
-        });
+function updateSelectedWindowPos() {
+    let total = 7 * window.innerHeight / 100; // 初始位置距离页面顶部的偏移（7vh）
+    windows.forEach((window, index) => {
+        const wh = window.offsetHeight; // 获取当前窗口的高度。
+        window.style.transition = "top 0.3s cubic-bezier(0.33, 1, 0.68, 1)";
+        // 更新每个窗口的位置，保持适当的间隔
+        window.style.top = `${total}px`; 
+        total += wh + 7; // 增加窗口高度和7px间距
+    });
+}
+
+// 创建窗口函数和关闭窗口函数。
+function create(window) {
+    windows.push(window);
+    updateSelectedWindowPos(); // 更新所有窗口位置。
+}
+
+function close(window) {
+    windows = windows.filter(win => win !== window);
+    updateSelectedWindowPos(); // 更新所有窗口位置。
+}
+
+function monitor() {
+    if (nullCount >= 3 && nullCount < 7) {
+        warning(`你已连续 ${nullCount} 次输入 null 或 undefined。请检查你所输入的内容。`)
+    } else if (nullCount >= 7 && nullCount < 14) {
+        warning(`再次警告！你已连续输入 null 或 undefined ${nullCount} 次。请检查你所输入的内容。`)
+    } else if (nullCount >= 14 && nullCount < 25) {
+        info(`null 和 undefined 是指，变量的值为 null 或 undefined，即你在 prompt 输入框中点击了取消或者在函数的参数中传入了 null 或 undefined。请检查你所输入的内容。`);
+    } else if (nullCount >= 25 && nullCount < 30) {
+        fail("你已被禁止调用任何函数。");
+        console.warn("你已被禁止调用任何函数。");
+        warning("请查看控制台。");
+    }
 }
 
 // info 函数。
-function info(string, name) {
-    num++;
+function info(string) {
+    if (string == null || string == undefined) {
+        nullCount++;
+        fail("所输入内容不能为 null 或 undefined。");
+        monitor();
+        return 39;
+    } if (nullCount > 26) {
+        return "你已被禁止调用函数。";
+    }
+
+    infoNum++;
     const window = document.createElement("div");
     window.className = "info-window";
     const square = document.createElement("div");
@@ -29,17 +75,13 @@ function info(string, name) {
     const content = document.createElement("div");
     content.className = "info-content";
 
-    updateSelectedWindowPos(".info-window"); // 更新所有窗口位置。
     document.body.appendChild(window);
     window.appendChild(square);
     window.appendChild(content);
     square.appendChild(icon);
+    create(window); // 添加窗口。
 
-    if (name === "" || name === undefined) {
-        name = "确定";
-    }
-
-    icon.innerHTML = `第 ${num} 条信息`;
+    icon.innerHTML = `第 ${infoNum} 条信息`;
     content.innerHTML = string;
 
     const line = Math.ceil(string.length / 14);
@@ -50,8 +92,7 @@ function info(string, name) {
         window.style.animation = "info- 0.7s forwards cubic-bezier(0.33, 1, 0.68, 1)";
         setTimeout(() => {
             document.body.removeChild(window);
-            updateSelectedWindowPos(".info-window"); // 更新所有窗口位置。
-            num--; // 更新窗口数量。
+            close(window) // 移除窗口。
         }, 700);
     }, 3000);
 }
@@ -59,7 +100,16 @@ function info(string, name) {
 // success 函数。
 
 function success(string) {
-    num++;
+    if (string == null || string == undefined) {
+        nullCount++;
+        fail("所输入内容不能为 null 或 undefined。");
+        monitor();
+        return 39;
+    } if (nullCount > 26) {
+        return "你已被禁止调用函数。";
+    }
+
+    successNum++;
     const window = document.createElement("div");
     window.className = "success-window";
     const square = document.createElement("div");
@@ -69,13 +119,13 @@ function success(string) {
     const content = document.createElement("div");
     content.className = "success-content";
 
-    updateSelectedWindowPos(".success-window"); // 更新所有窗口位置。
+    create(window); // 添加窗口。
     document.body.appendChild(window);
     window.appendChild(square);
     window.appendChild(content);
     square.appendChild(icon);
 
-    icon.innerHTML = `第 ${num} 条成功消息`;
+    icon.innerHTML = `第 ${successNum} 条成功消息`;
     content.innerHTML = string;
 
     const line = Math.ceil(string.length / 14);
@@ -86,8 +136,7 @@ function success(string) {
         window.style.animation = "success- 0.7s forwards cubic-bezier(0.33, 1, 0.68, 1)";
         setTimeout(() => {
             document.body.removeChild(window);
-            updateSelectedWindowPos(".success-window"); // 更新所有窗口位置。
-            num--; // 更新窗口数量。
+            close(window) // 移除窗口。
         }, 700);
     }, 3000);
 }
@@ -95,7 +144,16 @@ function success(string) {
 // fail 函数。
 
 function fail(string) {
-    num++;
+    if (string == null || string == undefined) {
+        nullCount++;
+        fail("所输入内容不能为 null 或 undefined。");
+        monitor();
+        return 39;
+    } if (nullCount > 26) {
+        return "你已被禁止调用函数。";
+    }
+
+    failNum++;
     const window = document.createElement("div");
     window.className = "fail-window";
     const square = document.createElement("div");
@@ -105,13 +163,13 @@ function fail(string) {
     const content = document.createElement("div");
     content.className = "fail-content";
 
-    updateSelectedWindowPos(".fail-window"); // 更新所有窗口位置。
+    create(window); // 添加窗口。
     document.body.appendChild(window);
     window.appendChild(square);
     window.appendChild(content);
     square.appendChild(icon);
 
-    icon.innerHTML = `第 ${num} 条错误消息`;
+    icon.innerHTML = `第 ${failNum} 条错误消息`;
     content.innerHTML = string;
 
     const line = Math.ceil(string.length / 14);
@@ -122,8 +180,7 @@ function fail(string) {
         window.style.animation = "fail- 0.7s forwards cubic-bezier(0.33, 1, 0.68, 1)";
         setTimeout(() => {
             document.body.removeChild(window);
-            updateSelectedWindowPos(".fail-window"); // 更新所有窗口位置。
-            num--; // 更新窗口数量。
+            close(window) // 移除窗口。
         }, 700);
     }, 3000);
 }
@@ -131,7 +188,16 @@ function fail(string) {
 // warning 函数。
 
 function warning(string) {
-    num++;
+    if (string == null || string == undefined) {
+        nullCount++;
+        fail("所输入内容不能为 null 或 undefined。");
+        monitor();
+        return 39;
+    } if (nullCount > 26) {
+        return "你已被禁止调用函数。";
+    }
+
+    warningNum++;
     const window = document.createElement("div");
     window.className = "warning-window";
     const square = document.createElement("div");
@@ -141,13 +207,13 @@ function warning(string) {
     const content = document.createElement("div");
     content.className = "warning-content";
 
-    updateSelectedWindowPos(".warning-window"); // 更新所有窗口位置。
+    create(window); // 添加窗口。
     document.body.appendChild(window);
     window.appendChild(square);
     window.appendChild(content);
     square.appendChild(icon);
 
-    icon.innerHTML = `第 ${num} 条警告消息`;
+    icon.innerHTML = `第 ${warningNum} 条警告消息`;
     content.innerHTML = string;
 
     const line = Math.ceil(string.length / 14);
@@ -158,8 +224,7 @@ function warning(string) {
         window.style.animation = "warning- 0.7s forwards cubic-bezier(0.33, 1, 0.68, 1)";
         setTimeout(() => {
             document.body.removeChild(window);
-            updateSelectedWindowPos(".warning-window"); // 更新所有窗口位置。
-            num--; // 更新窗口数量。
+            close(window) // 移除窗口。
         }, 700);
     }, 3000);
 }
@@ -168,7 +233,7 @@ function warning(string) {
 
 function input(string, holder) {
     return new Promise((resolve) => {
-        num++;
+        inputNum++;
         const window = document.createElement("div");
         window.className = "input-window";
         const square = document.createElement("div");
@@ -177,22 +242,29 @@ function input(string, holder) {
         icon.className = "input-icon";
         const content = document.createElement("div");
         content.className = "input-content";
-        if (holder === "" || holder === undefined) {
+        if (holder === "") {
             holder = "在此输入……";
+        } else if (string === null || string === undefined || holder === undefined || holder === null) {
+            nullCount++;
+            fail("所输入内容不能为 null 或 undefined。");
+            monitor();
+            return 39;
+        } if (nullCount > 26) {
+            return "你已被禁止调用函数。";
         }
         const box = document.createElement("input");
         box.type = "text";
         box.className = "input-box";
         box.placeholder = holder;
 
-        updateSelectedWindowPos(".input-window"); // 更新所有窗口位置。
+        create(window); // 添加窗口。
         document.body.appendChild(window);
         window.appendChild(square);
         window.appendChild(content);
         window.appendChild(box);
         square.appendChild(icon);
 
-        icon.innerHTML = `第 ${num} 项输入`;
+        icon.innerHTML = `第 ${inputNum} 项输入`;
         content.innerHTML = string;
 
         const line = Math.ceil(string.length / 14);
@@ -208,8 +280,7 @@ function input(string, holder) {
                     if (document.body.contains(window)) {
                         document.body.removeChild(window);
                         resolve(value);
-                        updateSelectedWindowPos(".input-window"); // 更新所有窗口位置。
-                        num--; // 更新窗口数量。
+                        close(); // 移除窗口。
                     }
                 }, 700);
             }
@@ -222,7 +293,7 @@ function input(string, holder) {
 
 function choice(string, name1, name2) {
     return new Promise((resolve) => {
-        num++;
+        choiceNum++;
         const window = document.createElement("div");
         window.className = "choice-window";
         const square = document.createElement("div");
@@ -231,18 +302,24 @@ function choice(string, name1, name2) {
         icon.className = "choice-icon";
         const content = document.createElement("div");
         content.className = "choice-content";
-        if (name1 === "" || name2 === "" || name1 === undefined || name2 === undefined) {
+        if (name1 === "" || name2 === "") {
             name1 = "确定";
             name2 = "取消";
+        } if (string == null || string == undefined || name1 === undefined || name2 === undefined || name1 === null || name2 === null) {
+            nullCount++;
+            fail("所输入内容不能为 null 或 undefined。");
+            monitor();
+            return 39;
+        } if (nullCount > 26) {
+            return "你已被禁止调用函数。";
         }
-
-        updateSelectedWindowPos(".choice-window"); // 更新所有窗口位置。
+        create(window); // 添加窗口。
         document.body.appendChild(window);
         window.appendChild(square);
         window.appendChild(content);
         square.appendChild(icon);
 
-        icon.innerHTML = `第 ${num} 项选择`;
+        icon.innerHTML = `第 ${choiceNum} 项选择`;
         content.innerHTML = string;
 
         const line = Math.ceil(string.length / 14);
@@ -254,7 +331,7 @@ function choice(string, name1, name2) {
         True.className = "choice-true";
         True.onclick = () => {
             resolve(true);
-            close();
+            rm();
         };
 
         const False = document.createElement("button");
@@ -262,19 +339,18 @@ function choice(string, name1, name2) {
         False.className = "choice-false";
         False.onclick = () => {
             resolve(false);
-            close();
+            rm();
         };
 
         // 将按钮添加到内容中。
         content.appendChild(True);
         content.appendChild(False);
 
-        function close() {
+        function rm() {
             window.style.animation = "choice- 0.7s forwards cubic-bezier(0.33, 1, 0.68, 1)"; // 播放出场动画
             setTimeout(() => {
                 document.body.removeChild(window);
-                updateSelectedWindowPos(".choice-window"); // 更新所有窗口位置。
-                num--; // 更新窗口数量。
+                close(window) // 移除窗口。
             }, 700);
         }
     });
@@ -283,7 +359,16 @@ function choice(string, name1, name2) {
 // transmit 函数。
 
 function transmit(string) {
-    num++;
+    if (string == null || string == undefined) {
+        nullCount++;
+        fail("所输入内容不能为 null 或 undefined。");
+        monitor();
+        return 39;
+    } if (nullCount > 26) {
+        return "你已被禁止调用函数。";
+    }
+
+    transmitNum++;
     const window = document.createElement("div");
     window.className = "transmit-window";
     const square = document.createElement("div");
@@ -293,13 +378,13 @@ function transmit(string) {
     const content = document.createElement("div");
     content.className = "transmit-content";
 
-    updateSelectedWindowPos(".command-window");
+    create(window); // 添加窗口。
     document.body.appendChild(window);
     window.appendChild(square);
     window.appendChild(content);
     square.appendChild(icon);
 
-    icon.innerHTML = `第 ${num} 次传输`;
+    icon.innerHTML = `第 ${transmitNum} 次传输`;
     content.innerHTML = string;
 
     const line = Math.ceil(string.length / 14);
@@ -310,8 +395,7 @@ function transmit(string) {
         window.style.animation = "transmit- 0.7s forwards cubic-bezier(0.33, 1, 0.68, 1)";
         setTimeout(() => {
             document.body.removeChild(window);
-            updateSelectedWindowPos(".transmit-window"); // 更新所有窗口位置。
-            num--; // 更新窗口数量。
+            close(window) // 移除窗口。
         }, 700);
     }, 3000);
 }
@@ -319,12 +403,20 @@ function transmit(string) {
 // link 函数。
 
 function link(string, url) {
-    if (url === "" || url === undefined) {
-        fail("链接地址不能为空！");
+    if (string == null || string == undefined || url == null || url == undefined) {
+        nullCount++;
+        fail("所输入内容不能为 null 或 undefined。");
+        monitor();
         return 39;
+    } else if (url.startsWith("https") != true) {
+        url = "https://" + url;
+    } else if (urlEndings.some(ending => url.endsWith(ending)) == false) {
+        warning("请检查你所输入的网址是否正确！");
+    } if (nullCount > 26) {
+        return "你已被禁止调用函数。";
     }
 
-    num++;
+    linkNum++;
     const window = document.createElement("div");
     window.className = "link-window";
     const square = document.createElement("div");
@@ -335,17 +427,19 @@ function link(string, url) {
     content.className = "link-content";
     const btn = document.createElement("button");
     btn.className = "link-btn";
+    var line_ = Math.ceil(url.length / 14);
+    console.log(line_);
+    content.style.marginBottom = `calc(70px + ${Math.round(line_ / 2)}em)`;
 
-
-    updateSelectedWindowPos(".link-window"); // 更新所有窗口位置。
+    create(window); // 添加窗口。
     document.body.appendChild(window);
     window.appendChild(square);
     window.appendChild(content);
     square.appendChild(icon);
 
-    icon.innerHTML = `第 ${num} 个链接`;
+    icon.innerHTML = `第 ${linkNum} 个链接`;
     content.innerHTML = string;
-    btn.innerHTML = "跳转";
+    btn.innerHTML = `跳转至 ${url}`;
     content.appendChild(btn);
 
     const line = Math.ceil(string.length / 14);
@@ -353,12 +447,11 @@ function link(string, url) {
     content.style.height = `${line * lineHeight}px`;
 
     btn.onclick = () => {
-        open(url, "_blank", "width=800, height=600")
+        open(url, "_blank", "width=1000, height=700")
         window.style.animation = "link- 0.7s forwards cubic-bezier(0.33, 1, 0.68, 1)";
         setTimeout(() => {
             document.body.removeChild(window);
-            updateSelectedWindowPos(".link-window"); // 更新所有窗口位置。
-            num--; // 更新窗口数量。
+            close(window) // 移除窗口。
         }, 700);
     }
 }
@@ -366,7 +459,16 @@ function link(string, url) {
 // command 函数。
 
 function command(string) {
-    num++;
+    if (string == null || string == undefined) {
+        nullCount++;
+        fail("所输入内容不能为 null 或 undefined。");
+        monitor();
+        return 39;
+    } if (nullCount > 26) {
+        return "你已被禁止调用函数。";
+    }
+
+    commandNum++;
     const window = document.createElement("div");
     window.className = "command-window";
     const square = document.createElement("div");
@@ -376,13 +478,13 @@ function command(string) {
     const content = document.createElement("div");
     content.className = "command-content";
 
-    updateSelectedWindowPos(".command-window"); // 更新所有窗口位置。
+    create(window); // 添加窗口。
     document.body.appendChild(window);
     window.appendChild(square);
     window.appendChild(content);
     square.appendChild(icon);
 
-    icon.innerHTML = `第 ${num} 个终端窗口`;
+    icon.innerHTML = `第 ${commandNum} 个终端窗口`;
     content.innerHTML = string;
 
     const line = Math.ceil(string.length / 14);
@@ -393,8 +495,7 @@ function command(string) {
         window.style.animation = "command- 0.7s forwards cubic-bezier(0.33, 1, 0.68, 1)";
         setTimeout(() => {
             document.body.removeChild(window);
-            updateSelectedWindowPos(".command-window"); // 更新所有窗口位置。
-            num--; // 更新窗口数量。
+            close(window) // 移除窗口。        
         }, 700);
     }, 3000);
 }
