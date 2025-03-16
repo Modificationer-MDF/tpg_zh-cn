218aededc515850fd794c0efe2915637aba04cd3
Feng_Noti <Feng_14@outlook.com>
2025-03-16 09:59:08 +0800

    1
    1

--- js/global.js
+++ js/global.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const ctrl = document.querySelector(".control-pad");
+    ctrl.style.animationName = "-ctrl";
     function width(name) {
         const el = document.querySelector(name);
         el.style.width = window.innerWidth + "px";
@@ -7,9 +8,13 @@ document.addEventListener("DOMContentLoaded", function () {
     setInterval(() => {
         width(".top");
         fn5();
-        document.addEventListener("keydown", (event) => {
-            if (event.ctrlKey) ctrl.style.animation = `ctrl- 0.7s forwards ${easing}`;
-            else if (event.shiftKey) ctrl.style.animation = `-ctrl 0.7s forwards ${easing}`;
-        });
     }, 140);
+    document.addEventListener("keydown", (event) => {
+        if (event.ctrlKey) ctrl.style.animation = `ctrl- 0.7s forwards ${easing}`;
+        else if (event.shiftKey) ctrl.style.animation = `-ctrl 0.7s forwards ${easing}`;
+        else if (event.altKey) {
+            if (ctrl.style.animationName === "-ctrl") info("按下 Ctrl 键打开控制台。");
+            else if (ctrl.style.animationName === "ctrl-") info("按下 Shift 键关闭控制台。");
+        }
+    });
 });
